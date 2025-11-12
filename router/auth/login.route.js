9ad47ee0926d795a69fe624fed54f3b0ddf58b56
Dennis Peprah <macbookpro@Dennis-MacBook-Pro-2.local>
2025-11-12 14:52:10 +0000

    Updated login route

--- router/auth/login.route.js
+++ router/auth/login.route.js
@@ -1,9 +1,8 @@
 import LoginController from "../../controller/auth/login.controller.js";
-import Authentication from "../../middleware/auth.js"
 import express from "express";
 
 const router = express.Router();
 
-router.post("/login", Authentication (next),LoginController.userLogin);
+router.post("/login", LoginController.userLogin);
 
 export default router;
